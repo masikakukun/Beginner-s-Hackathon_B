bf9d8a403052aa84ff7be524548582c774ae07f0
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2024-03-16 15:17:21 +0900

    Javascriptのフロントエンドの作成

---
+++ javascript/frontend.js
@@ -0,0 +1,24 @@
+document.title="フロントエンド"
+
+//洗濯機の数
+const washer_num=6
+//洗濯機の空き状況
+var washer_status=new Array(washer_num);
+washer_status.fill("0");
+console.log(washer_status);
+
+//それぞれの洗濯機の空き状況
+for(let i=0;i<washer_num;i++){
+	if (washer_status[i]=="0"){
+	document.write("<p>洗濯機"+(i+1)+":"+"空き"+"</p>");
+	}else {
+	document.write("<p>洗濯機"+(i+1)+":"+"使用中 残り"+washer_status[i]+"分"+"</p>");
+	}
+}
+
+//更新ボタンを押したときの処理
+function onRefresh(){
+	//ページをリロード
+	location.reload();
+	alert("更新しました")
+}
