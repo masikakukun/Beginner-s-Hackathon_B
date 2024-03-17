58fecb36f612169ea696e6c2a220e89df9c93798
masikakukun <122787340+masikakukun@users.noreply.github.com>
2024-03-17 13:45:27 +0900

    テストケース2

---
+++ testcase2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<time.h>
+#include<chrono>
+#include<unistd.h>
+using namespace std;
+int main(){
+    srand((unsigned int)time(NULL));
+    for (int i=1;i<=100;i++){
+        int q = rand()%2+1;
+        char query;
+        if (q==1){
+            query = 'A';
+            int N = rand()%100+1;
+            int M = rand()%10+1;
+            int T = rand()%100+1;
+            cout << query << " " << N << " " << M << " " << T << endl;
+        }
+        if (q==2){
+            query = 'B';
+            int N = rand()%100+1;
+            cout << query << " " << N << endl;
+        }
+        int t = rand()%3600+1;
+        sleep(t);
+    }
+    return 0;
+}
