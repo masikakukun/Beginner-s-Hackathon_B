462a0083d05641432e00e4d8c52a86ae37e7687e
sawadasota <163627344+sawadasota@users.noreply.github.com>
2024-03-17 14:13:26 +0900

    Create random.cpp

---
+++ random.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<time.h>
+#include<chrono>
+#include<unistd.h>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ofstream writing_file;
+    string filename ="query.txt";
+    writing_file.open(filename, std::ios::out);
+    srand((unsigned int)time(NULL));
+    for (int i=1;i<=100;i++){
+        int q = rand()%2+1;
+        char query;
+        if (q==1){
+            query = 'A';
+            int N = rand()%100+1;
+            int M = rand()%10+1;
+            int T = rand()%100+1;
+            writing_file << query << " " << N << " " << M << " " << T << endl;
+        }
+        if (q==2){
+            query = 'B';
+            writing_file << query << endl;        
+        }
+        int t = rand()%3600+1;
+        sleep(t);
+    }
+    return 0;
+    
+
+}
